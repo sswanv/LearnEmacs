f4301199c79cc660b62db8bfed85da6fa61b98f2
sswanv <sswanv@gmail.com>
2016-10-22 15:43:13 +0800

    day05.

--- lisp/custom.el
+++ lisp/custom.el
@@ -3,7 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(company-minimum-prefix-length 1))
+ '(company-minimum-prefix-length 1)
+ '(popwin:popup-window-position (quote right))
+ '(popwin:popup-window-width 120))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -18,6 +18,10 @@
 			  exec-path-from-shell
 			  popwin
 			  reveal-in-osx-finder
+			  web-mode
+			  js2-refactor
+			  expand-region
+			  iedit
 			  )  "Default packages")
 
 (setq package-selected-packages sswanv/packages)
@@ -40,13 +44,15 @@
 (global-hungry-delete-mode t)
 
 (smartparens-global-mode t)
+(sp-local-pair 'emacs-lisp-mode "'" nil :actions nil)
 
 (ivy-mode 1)
 (setq ivy-use-virtual-buffers t)
 
 (setq auto-mode-alist
       (append
-       '(("\\.js\\'" . js2-mode))
+       '(("\\.js\\'" . js2-mode)
+	 ("\\.html\\'" . web-mode))
        auto-mode-alist))
 
 (require 'popwin)
@@ -55,5 +61,34 @@
 (load-theme 'monokai t)
 
 (global-company-mode t)
+
+(add-hook 'web-mode-hook 'my-web-mode-indent-setup)
+
+(defun my-toggle-web-indent ()
+  (interactive)
+  ;; web development
+  (if (or (eq major-mode 'js-mode) (eq major-mode 'js2-mode))
+      (progn
+	(setq js-indent-level (if (= js-indent-level 2) 4 2))
+	(setq js2-basic-offset (if (= js2-basic-offset 2) 4 2))))
+
+  (if (eq major-mode 'web-mode)
+      (progn (setq web-mode-markup-indent-offset (if (= web-mode-markup-indent-offset 2) 4 2))
+	     (setq web-mode-css-indent-offset (if (= web-mode-css-indent-offset 2) 4 2))
+	     (setq web-mode-code-indent-offset (if (= web-mode-code-indent-offset 2) 4 2))))
+  (if (eq major-mode 'css-mode)
+      (setq css-indent-offset (if (= css-indent-offset 2) 4 2)))
+
+  (setq indent-tabs-mode nil))
+
+(global-set-key (kbd "C-c t i") 'my-toggle-web-indent)
+
+(add-hook 'js2-mode-hook #'js2-refactor-mode)
+(js2r-add-keybindings-with-prefix "C-c C-m")
+
+(global-set-key (kbd "M-s i") 'counsel-imenu)
+
+(global-set-key (kbd "C-=") 'er/expand-region)
+(global-set-key (kbd "M-s e") 'iedit-mode)
 
 (provide 'init-packages)
